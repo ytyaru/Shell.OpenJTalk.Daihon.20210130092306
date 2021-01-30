acc4b9c49874eaa8cf1fef3994ebc5e7127d34f8
ytyaru <yttry0@gmail.com>
2021-01-30 14:33:06 +0900

    serif

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,5 @@
 window.addEventListener('load', (event) => {
     console.log('HelloJS');
     Player.addEventListener();
-    /*
-    // https://developer.mozilla.org/ja/docs/Web/API/HTMLMediaElement/ended_event
-    const audioTags = document.querySelectorAll('audio');
-    const REGEXP_PATH = /(.+)\/([0-9]+)\.(wav|mp3|ogg|flac)/
-    for (let audioTag of audioTags) {
-        audioTag.addEventListener('ended', (event) => {
-            const oldSrc = event.target.src;
-            let index = 0;
-            event.target.src = event.target.src.replace(REGEXP_PATH, (match, dir, name, ext)=>{
-                index = Number(name);
-                index++;
-                if (Number(event.target.getAttribute('last')) < index) { index = 0; }
-                return dir + '/' + index + '.' + ext;
-            });
-            console.log(index);
-            if (0 < index) { event.target.play(); }
-        });
-    }
-    */
 });
 
